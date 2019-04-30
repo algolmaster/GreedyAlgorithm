c968e4242ff282fdce3af877ca61de3c159eae6a
신예지 <gjsld1@naver.com>
2019-05-01 00:51:39 +0900

    19/05/01 participateOrder

---
+++ participateOrder.cpp
@@ -0,0 +1,48 @@
+//
+//  participateOrder.cpp
+//  GreedyAlgorithm
+//
+//  Created by 신예지 on 30/04/2019.
+//  Copyright © 2019 신예지. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int tc;
+    cin >> tc;
+    for(int k=0; k<tc; k++) {
+        int n;
+        cin >> n;
+        
+        vector<int> teamA;
+        vector<int> teamB;
+        
+        int maxCompetiton = 0;
+        
+        int num;
+        for(int i=0; i<n; i++) {
+            cin >> num;
+            teamA.push_back(num);
+        }
+        for(int i=0; i<n; i++) {
+            cin >> num;
+            teamB.push_back(num);
+        }
+        sort(teamA.begin(), teamA.end());
+        sort(teamB.begin(), teamB.end());
+        
+        for(int i=0; i<n; i++) {
+            for(vector<int>::iterator iter=teamB.begin(); iter!=teamB.end(); iter++) {
+                if(teamA[i]<=*iter) {
+                    teamB.erase(iter);
+                    maxCompetiton++;
+                    iter--;
+                }
+            }
+        }
+        cout << maxCompetiton << endl;
+    }
+}
