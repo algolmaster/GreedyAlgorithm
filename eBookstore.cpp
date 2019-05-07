11e4959c19ad42193af0982822dcc3fff89cab10
신예지 <gjsld1@naver.com>
2019-05-08 05:18:39 +0900

    19/05/01 eBookstore

--- eBookstore.cpp
+++ eBookstore.cpp
@@ -10,34 +10,47 @@
 #include <vector>
 #include <utility>
 #include <functional>
+#include <algorithm>
 using namespace std;
 
 int main() {
-    int n, m;
-    cin >> n >> m;
-    
-    int** menu = new int*[n];
-    for(int i=0; i<n; i++) {
-        menu[i] = new int[2*m];
-        for(int j=0; j<2*m; j++) cin >> menu[i][j];
-    }
-    
-    int* price = new int[m];
-    for(int i=0; i<m; i++) {
-        vector<pair<int, int>> vec;
-        int book=0, point=0;
+    int tc;
+    cin >> tc;
+    for(int l=0; l<tc; l++) {
+        int n, m;
+        cin >> n >> m;
         
-        for(int k=0; k<n; k++)
-            vec.push_back(pair<int, int>(menu[k][2*i+1],menu[k][2*i]));
-        sort(vec.begin(),vec.end(), greater<pair<int, int>>());
+        int** menu = new int*[n];
+        for(int i=0; i<n; i++) {
+            menu[i] = new int[2*m];
+            for(int j=0; j<2*m; j++) cin >> menu[i][j];
+        }
         
-        for(int k=0; k<n-1; k++) point += vec[k].first;
-        for(int k=0; k<n; k++) book += vec[k].second;
-        price[i] = book-point;
+        vector<int> finalPay;
+        for(int i=0; i<m; i++) {
+            vector<pair<int, int>> vec;
+            int min =0;
+            for(int k=0; k<n; k++) {
+                vec.push_back(pair<int, int>(menu[k][2*i+1],menu[k][2*i]));
+                min += menu[k][2*i];
+            }
+            sort(vec.begin(),vec.end(), greater<pair<int, int>>());
+            
+            int pay = vec[0].second;
+            int rest = vec[0].first;
+            for(int j=1; j<n; j++) {
+                if(vec[j].second <= rest) {
+                    rest -= vec[j].second;
+                    rest += vec[j].first;
+                }
+                else {
+                    pay += vec[j].second-rest;
+                    rest = vec[j].first;
+                }
+            }
+            finalPay.push_back(pay);
+        }
+        
+        cout << *min_element(finalPay.begin(), finalPay.end());
     }
-    
-    int smallPrice = price[0];
-    for(int i=1; i<m; i++) if(smallPrice>price[i]) smallPrice=price[i];
-    
-    cout << smallPrice;
 }
